49fa159ba31224ce66677153538188d1c0ae5f98
Mustafa <aydomus3569@hotmail.com>
2023-11-25 14:19:03 +0100

    feat(quizzes): création du filtre pour la page quizzes

--- frontend/src/app/components/quizzes/quizzes.component.ts
+++ frontend/src/app/components/quizzes/quizzes.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
+import { User } from 'oidc-client';
+import { MatTableDataSource } from '@angular/material/table';
+import { Quiz } from 'src/app/models/quiz';
+import { MatTableState } from 'src/app/helpers/mattable.state';
+import { StateService } from 'src/app/services/state.service';
 
 
 @Component({
@@ -8,14 +13,32 @@ import { AuthenticationService } from '../../services/authentication.service';
     templateUrl: 'quizzes.component.html'
 })
 export class QuizzesComponent{
+    dataSource: MatTableDataSource<Quiz> = new MatTableDataSource();
+    filter : string = '';
+    state: MatTableState;
+
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private stateService: StateService
     ) {
         // redirect to home if not logged
         if (!this.authenticationService.currentUser) {
             this.router.navigate(['/restricted']);
         }
+        this.state = this.stateService.quizListState;
+    }
+
+    filterChanged(e: KeyboardEvent) {
+        const filterValue = (e.target as HTMLInputElement).value;
+        // applique le filtre au datasource (et provoque l'utilisation du filterPredicate)
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+        // sauve le nouveau filtre dans le state
+        this.state.filter = this.dataSource.filter;
+        // comme le filtre est modifié, les données aussi et on réinitialise la pagination
+        // en se mettant sur la première page
+        if (this.dataSource.paginator)
+            this.dataSource.paginator.firstPage();
     }
 }
---
+++ frontend/src/app/models/quiz.ts
@@ -0,0 +1,13 @@
+
+export class Quiz {
+    id?: number;
+    name?: string;
+    description?: string;
+    isPublished?: boolean;
+    isClosed?: boolean;
+    isTest?: boolean;
+    startDate?: Date;
+    endDate?: Date;
+    databaseId?: number;
+
+}
