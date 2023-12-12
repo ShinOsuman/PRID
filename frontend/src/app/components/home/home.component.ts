5f58dc01b29fa577929d9f9fdc88b5a4f65af8d4
Mustafa Aydogan <aydomus3569@hotmail.com>
2023-12-12 23:42:36 +0100

    fix(home): ajout de la redirection vers la page quizzes si user déjà connecté

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUser) {
+        this.router.navigate(['/quizzes']);
+    }
+}
 }
